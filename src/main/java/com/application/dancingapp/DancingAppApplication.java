9489cd441820b8e4f899f9a0b6631ec69dd1e604
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2024-01-14 14:57:59 +0200

    Initial commit

---
+++ src/main/java/com/application/dancingapp/DancingAppApplication.java
@@ -0,0 +1,13 @@
+package com.application.dancingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DancingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/dancingapp/DancingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.dancingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DancingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
